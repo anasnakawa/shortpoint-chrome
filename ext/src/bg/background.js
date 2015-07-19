bd99b59d17fc463c18829795e6f62225e892a94d
Anas Nakawa <anas.nakawa@gmail.com>
2015-07-19 16:29:08 +0400

    change log buttons

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,7 +1,7 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+// if you checked 'fancy-settings' in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
+// var settings = new Store('settings', {
+//     'sample_setting': 'This is how you use Store.js to remember values'
 // });
 
 //example of using a message handler from the inject scripts
@@ -29,9 +29,24 @@ function notify( title, message, delay ) {
       type: 'basic',
       title: title,
       message: message,
-      iconUrl: '../../icons/icon128.png'
+      iconUrl: '../../icons/icon128.png',
+      buttons: [{
+          title: 'Foundation changelog'
+      }, {
+          title: 'Office365 changelog'
+      }]
     }, function() {
       console.log( 'notification is created' );
     })
   }, delay || 1);
 }
+
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+  // if (notifId === myNotificationID) {
+  if (btnIdx === 0) {
+    window.open( 'https://docs.google.com/document/d/1ihBMWlZFrLQtZI1vrhC5juqkC4jEWN_AOGi20mrWc8s/edit' );
+  } else if (btnIdx === 1) {
+    window.open( 'https://docs.google.com/document/d/1Mlarb6f-QnWn_p4GLtXFKluTyw8vkOi-maJLUwenY5Q/edit' )
+  }
+  // }
+});
